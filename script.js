925b4cf58124667122475f6df3f182ad905dabf5
Shubham Pandey <p-shubham1996@users.noreply.github.com>
2017-01-20 12:34:32 +0530

    Updated

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+var app = angular.module("app",['ngRoute']);
+app.config(['$routeProvider',function($routeProvider){
+  $routeProvider
+  .when('/main',{
+    templateUrl: 'main.html',
+    controller: 'MainCtrl'
+  })
+  .when('/about',{
+    templateUrl: 'about.html',
+    controller: 'MainCtrl'
+  })
+  .when('/services',{
+    templateUrl: 'services.html',
+    controller: 'ServicesCtrl'
+  })
+  .when('/contact',{
+    templateUrl: 'contact.html',
+    controller: 'ContactCtrl'
+  })
+  .otherwise({redirectTo:'/main'})
+}]);
 
+app.controller('MainCtrl',['$scope',function($scope){
+  console.log($scope);
+}]);
+
+app.controller('ServicesCtrl',['$scope','$http',function($scope,$http){
+  $http.get('services.json').then(function(response){
+    $scope.services = response.data;
+    console.log(response.data);
+  })
+}]);
+
+app.controller('ContactCtrl',['$scope',function($scope){
+  console.log($scope);
+}]);
